d999baff5fc56371298eab4396c61ba16db3c2d5
Pradeep Rawate <pradeep.rawate.ext@siemens.com>
2018-09-12 14:44:19 +0530

    Demo for ng6-pagination

---
+++ assets/data.ts
@@ -0,0 +1,404 @@
+export const people = [
+    {
+        'Name': 'Herrod Hancock',
+        'Company': 'Nulla Integer Vulputate Corp.'
+    },
+    {
+        'Name': 'Kasper Campos',
+        'Company': 'Etiam Ligula Associates'
+    },
+    {
+        'Name': 'Hamilton Kramer',
+        'Company': 'Interdum Ligula Eu Industries'
+    },
+    {
+        'Name': 'Tobias House',
+        'Company': 'Ut Dolor Foundation'
+    },
+    {
+        'Name': 'Boris Pena',
+        'Company': 'Aenean Eget PC'
+    },
+    {
+        'Name': 'Tarik Cook',
+        'Company': 'Quis Accumsan Convallis LLP'
+    },
+    {
+        'Name': 'Gabriel Mckenzie',
+        'Company': 'Vulputate Corp.'
+    },
+    {
+        'Name': 'Nash Bowers',
+        'Company': 'Et Lacinia Corp.'
+    },
+    {
+        'Name': 'Brenden Clemons',
+        'Company': 'Orci Ut Inc.'
+    },
+    {
+        'Name': 'Kasper Solomon',
+        'Company': 'Congue Limited'
+    },
+    {
+        'Name': 'Bruno Burt',
+        'Company': 'Nulla Ltd'
+    },
+    {
+        'Name': 'Branden Fowler',
+        'Company': 'Nulla At LLC'
+    },
+    {
+        'Name': 'Clark Fields',
+        'Company': 'Magna Et LLC'
+    },
+    {
+        'Name': 'Ferris Puckett',
+        'Company': 'Eu Dui Cum Ltd'
+    },
+    {
+        'Name': 'Aidan Levy',
+        'Company': 'Euismod Est Arcu Institute'
+    },
+    {
+        'Name': 'Malcolm Berry',
+        'Company': 'Quam LLP'
+    },
+    {
+        'Name': 'Honorato Sullivan',
+        'Company': 'Pellentesque Massa Lobortis PC'
+    },
+    {
+        'Name': 'Clark Wiggins',
+        'Company': 'Vel Est Tempor Industries'
+    },
+    {
+        'Name': 'Cole Estrada',
+        'Company': 'Amet Consectetuer Adipiscing Corporation'
+    },
+    {
+        'Name': 'Preston Sosa',
+        'Company': 'Est Ac Facilisis Limited'
+    },
+    {
+        'Name': 'Gil Forbes',
+        'Company': 'Lorem Lorem LLC'
+    },
+    {
+        'Name': 'Baxter Gaines',
+        'Company': 'Magnis Dis Parturient Inc.'
+    },
+    {
+        'Name': 'Keaton Berg',
+        'Company': 'Diam Vel Foundation'
+    },
+    {
+        'Name': 'Tucker Battle',
+        'Company': 'Sit Amet Consulting'
+    },
+    {
+        'Name': 'Reece Chase',
+        'Company': 'In Associates'
+    },
+    {
+        'Name': 'Kieran Adkins',
+        'Company': 'Risus In Foundation'
+    },
+    {
+        'Name': 'Salvador Cardenas',
+        'Company': 'Aliquam Erat Consulting'
+    },
+    {
+        'Name': 'Drake Hinton',
+        'Company': 'Nec Tempus Institute'
+    },
+    {
+        'Name': 'Merritt Buckley',
+        'Company': 'Donec LLP'
+    },
+    {
+        'Name': 'Harding Holden',
+        'Company': 'Eget Venenatis A LLC'
+    },
+    {
+        'Name': 'Kasimir Hatfield',
+        'Company': 'Sed Orci Lobortis Corp.'
+    },
+    {
+        'Name': 'Macon Howell',
+        'Company': 'Malesuada PC'
+    },
+    {
+        'Name': 'Howard Mercado',
+        'Company': 'Nulla Ltd'
+    },
+    {
+        'Name': 'Amery Hobbs',
+        'Company': 'Magnis Dis Corp.'
+    },
+    {
+        'Name': 'Walker Hess',
+        'Company': 'Nibh PC'
+    },
+    {
+        'Name': 'Michael Harrington',
+        'Company': 'Scelerisque Neque Nullam Corporation'
+    },
+    {
+        'Name': 'Shad Wall',
+        'Company': 'Lacus Quisque Incorporated'
+    },
+    {
+        'Name': 'Walker Hampton',
+        'Company': 'Rhoncus Nullam Consulting'
+    },
+    {
+        'Name': 'Dillon Snyder',
+        'Company': 'Ultrices Sit Foundation'
+    },
+    {
+        'Name': 'Gray Joseph',
+        'Company': 'At Arcu Vestibulum Institute'
+    },
+    {
+        'Name': 'Octavius Castro',
+        'Company': 'Arcu Incorporated'
+    },
+    {
+        'Name': 'Hop Richardson',
+        'Company': 'Suspendisse Industries'
+    },
+    {
+        'Name': 'Christopher Shaw',
+        'Company': 'Et Rutrum Eu LLP'
+    },
+    {
+        'Name': 'Bruce Tyson',
+        'Company': 'Fermentum Arcu Vestibulum Limited'
+    },
+    {
+        'Name': 'Arthur Emerson',
+        'Company': 'Tortor Nunc Consulting'
+    },
+    {
+        'Name': 'Theodore Velez',
+        'Company': 'Convallis Dolor Quisque Ltd'
+    },
+    {
+        'Name': 'Craig Stokes',
+        'Company': 'Laoreet LLC'
+    },
+    {
+        'Name': 'Ralph Diaz',
+        'Company': 'Amet Corp.'
+    },
+    {
+        'Name': 'Daquan Montgomery',
+        'Company': 'Elit Erat Associates'
+    },
+    {
+        'Name': 'Elmo Bonner',
+        'Company': 'Non Lorem Ltd'
+    },
+    {
+        'Name': 'Cyrus Summers',
+        'Company': 'Metus Inc.'
+    },
+    {
+        'Name': 'Dorian Salazar',
+        'Company': 'Aliquet Molestie Tellus Associates'
+    },
+    {
+        'Name': 'Arthur Shepherd',
+        'Company': 'Nunc LLP'
+    },
+    {
+        'Name': 'Walter Williams',
+        'Company': 'Sapien Ltd'
+    },
+    {
+        'Name': 'Brett Best',
+        'Company': 'Erat Corp.'
+    },
+    {
+        'Name': 'Jarrod Aguilar',
+        'Company': 'Ipsum Primis In Consulting'
+    },
+    {
+        'Name': 'Dexter Townsend',
+        'Company': 'Nam Interdum PC'
+    },
+    {
+        'Name': 'Lamar Carey',
+        'Company': 'Proin Nisl Sem Institute'
+    },
+    {
+        'Name': 'Shad Maxwell',
+        'Company': 'Rutrum Magna Cras Corporation'
+    },
+    {
+        'Name': 'Wyatt Mcclain',
+        'Company': 'Aliquet Inc.'
+    },
+    {
+        'Name': 'Tyler Pruitt',
+        'Company': 'Nam Corp.'
+    },
+    {
+        'Name': 'David Cantu',
+        'Company': 'Quam Foundation'
+    },
+    {
+        'Name': 'Branden Madden',
+        'Company': 'Cum Industries'
+    },
+    {
+        'Name': 'Theodore Fischer',
+        'Company': 'Dui Suspendisse Corp.'
+    },
+    {
+        'Name': 'Hasad Mclaughlin',
+        'Company': 'Tristique Ac LLP'
+    },
+    {
+        'Name': 'Phelan Spencer',
+        'Company': 'Sagittis Industries'
+    },
+    {
+        'Name': 'Hammett Vincent',
+        'Company': 'Mauris Ipsum Porta Institute'
+    },
+    {
+        'Name': 'Kane Swanson',
+        'Company': 'Sociis Corp.'
+    },
+    {
+        'Name': 'Nash Kidd',
+        'Company': 'Phasellus Dolor Incorporated'
+    },
+    {
+        'Name': 'Adam Lancaster',
+        'Company': 'Ipsum Consulting'
+    },
+    {
+        'Name': 'Oren Guthrie',
+        'Company': 'Ac Facilisis Ltd'
+    },
+    {
+        'Name': 'Rooney Howe',
+        'Company': 'Nullam Lobortis Quam Corp.'
+    },
+    {
+        'Name': 'Jacob Maynard',
+        'Company': 'Fusce Feugiat Lorem Limited'
+    },
+    {
+        'Name': 'Timon Baxter',
+        'Company': 'Molestie Pharetra Nibh Limited'
+    },
+    {
+        'Name': 'Flynn Caldwell',
+        'Company': 'Consequat Consulting'
+    },
+    {
+        'Name': 'Gannon Clemons',
+        'Company': 'Ridiculus Mus Proin PC'
+    },
+    {
+        'Name': 'Jordan Cote',
+        'Company': 'Arcu Vel Foundation'
+    },
+    {
+        'Name': 'Alden Nieves',
+        'Company': 'Lobortis Quam Industries'
+    },
+    {
+        'Name': 'Jermaine Burch',
+        'Company': 'Gravida Sagittis Duis Inc.'
+    },
+    {
+        'Name': 'Deacon Klein',
+        'Company': 'Pede Sagittis Associates'
+    },
+    {
+        'Name': 'Emery Sheppard',
+        'Company': 'Cubilia Curae; Company'
+    },
+    {
+        'Name': 'Ignatius Stevenson',
+        'Company': 'Turpis Nulla Consulting'
+    },
+    {
+        'Name': 'Aristotle Hall',
+        'Company': 'Magna Sed Eu PC'
+    },
+    {
+        'Name': 'Orlando Rutledge',
+        'Company': 'Natoque Penatibus Et PC'
+    },
+    {
+        'Name': 'Leonard Murray',
+        'Company': 'Vitae Incorporated'
+    },
+    {
+        'Name': 'Chaim Adams',
+        'Company': 'Augue Ac Ipsum LLC'
+    },
+    {
+        'Name': 'Joshua Bowers',
+        'Company': 'Facilisis Non LLC'
+    },
+    {
+        'Name': 'Colby Alvarez',
+        'Company': 'Integer Sem Elit Limited'
+    },
+    {
+        'Name': 'Eagan Leon',
+        'Company': 'Etiam Vestibulum Massa Associates'
+    },
+    {
+        'Name': 'Bruno Ellison',
+        'Company': 'Ac LLP'
+    },
+    {
+        'Name': 'Raymond Campbell',
+        'Company': 'Ipsum Corp.'
+    },
+    {
+        'Name': 'Kyle Coleman',
+        'Company': 'In Consequat Enim Ltd'
+    },
+    {
+        'Name': 'Chaney Rice',
+        'Company': 'Ut Quam Limited'
+    },
+    {
+        'Name': 'Leonard Morin',
+        'Company': 'Metus Foundation'
+    },
+    {
+        'Name': 'Quentin Whitney',
+        'Company': 'Non Ltd'
+    },
+    {
+        'Name': 'Griffith Edwards',
+        'Company': 'Sem Mollis Dui Company'
+    },
+    {
+        'Name': 'Mohammad Gates',
+        'Company': 'Tempus LLP'
+    },
+    {
+        'Name': 'Chester Ramirez',
+        'Company': 'Ac Metus Vitae Ltd'
+    },
+    {
+        'Name': 'Theodore Tyson',
+        'Company': 'Aliquet LLP'
+    },
+    {
+        'Name': 'Oleg Valenzuela',
+        'Company': 'Semper Erat PC'
+    }
+];
+
+
